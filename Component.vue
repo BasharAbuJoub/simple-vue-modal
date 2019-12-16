6278bfb9dfbd6251fdda6cced796a8ae4e7ff812
Bashar Abu Joub <bashar.abujoub@gmail.com>
2019-12-16 18:58:11 +0200

    Clean code

--- Component.vue
+++ Component.vue
@@ -1,14 +1,17 @@
 <template>
-    <div :id="name" class="overlay">
-        <a @click.prevent="closeModal" class="cancel"></a>
+    <div>
+        <button @click.prevent="show" class="btn">Show</button>
+        <div :id="name" class="modal-overlay" :class="{'modal-active' : active}">
+            <a @click.prevent="hide" class="modal-cancel"></a>
 
-        <div class="modal">
-            <slot></slot>
-            <a href="#" class="close">&times;</a>
+            <div class="modal-box">
+                <slot></slot>
+                <button @click.prevent="hide" class="modal-close">&times;</button>
 
-            <footer class="tw-mt-6">
-                <slot name="footer"></slot>
-            </footer>
+                <footer class="tw-mt-6">
+                    <slot name="footer"></slot>
+                </footer>
+            </div>
         </div>
     </div>
 </template>
@@ -16,10 +19,26 @@
 <script>
 export default {
     props: ['name'],
+    data: ()=>{
+        return {
+            active: false,
+        }
+    },
     methods: {
-        closeModal(){
-            window.location.hash = '';
+        show(){
+            this.active = true;
+            if(this.$activeModal != null)
+                this.$activeModal.hide();
+            this.$activeModal = this;
+            document.querySelector('body').classList.add('stop-scroll');
+
         },
+        hide(){
+            this.active = false;
+            this.$activeModal = null;
+            document.querySelector('body').classList.remove('stop-scroll');
+
+        }
 
     }
 }
@@ -29,26 +48,50 @@ export default {
     footer:empty{
         display: none;
     }
-    .overlay{
+    .modal-btn{
+
+    }
+    .modal-overlay{
         -webkit-backdrop-filter: blur(5px);
         backdrop-filter: blur(5px);
         background-color: rgba(0, 0, 0, 0.4);
         opacity: 0;
-        @apply tw-p-6 tw-flex tw-fixed tw-overflow-hidden tw-top-0 tw-right-0 tw-left-0 tw-bottom-0 tw-items-center tw-justify-center tw-invisible;
+        padding: 1.5rem;
+        display: flex;
+        position: fixed;
+        overflow: hidden;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        align-items: center;
+        justify-content: center;
+        visibility: hidden;
         transition: visibility 0.2s ease, opacity 0.2s ease-in;
     }
 
-    .overlay:target{
+    .stop-scroll{
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    @media screen and (min-width: 1024px){
+        .stop-scroll{
+            padding-right:17px;
+        }
+    }
+
+    .modal-overlay.modal-active{
         @apply tw-visible tw-opacity-100;
     }
 
-    .overlay .cancel{
+    .modal-overlay .modal-cancel{
 
         @apply tw-absolute tw-w-full tw-h-full;
 
     }
 
-    .modal{
+    .modal-box{
         transform: scale(0);
         opacity: 0;
         transition: transform ease-out 0.2s, opacity ease-out 0.2s;
@@ -56,17 +99,13 @@ export default {
             tw-p-10 tw-overflow-auto tw-max-h-full;
     }
 
-    body + .overlay:target{
-        height: 100vh;
-        overflow: hidden;
-    }
 
-    .overlay:target > .modal{
+    .modal-active > .modal-box{
         transform: scale(1);
         opacity: 1;
     }
 
-    .modal .close{
+    .modal-box .modal-close{
         @apply tw-absolute tw-mt-2 tw-mr-4 tw-top-0 tw-right-0 tw-text-2xl;
     }
 </style>
--- ModalPlugin.js
+++ ModalPlugin.js
@@ -1,6 +1,7 @@
 let Plugin = {
     install: function(Vue, options = {}){
         Vue.component('modal', require('./Component.vue').default);
+        Vue.prototype.$activeModal = null;
     }
 };
 
